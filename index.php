9aa00269c7cc26c2e5d9846643d4dc457249bbe3
cydharrell <cyd@codeforamerica.org>
2014-02-12 16:04:36 -0800

    added another alpha to breadcrumb

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <nav class="nav-breadcrumbs" role="navigation">
         <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="<?= $context->base() ?>/">Library</a></li>
+            <li><a href="<?= $context->base() ?>/">Library (Alpha)</a></li>
         </ul>
     </nav>
 	
